fcd9de08d36cefcc9c49c42d3dd1e2082de28df8
Tara Martin <martin.tara117@gmail.com>
2021-02-01 21:34:09 -0500

    made changes to file structure

--- client/src/containers/search-results/search-results.jsx
+++ client/src/containers/search-results/search-results.jsx
@@ -1,3 +1,11 @@
-import React from "react";
+import React from "./node_modules/react";
+
+const searchResults = () => (
+    <Container title="Search">
+     <Header />
+     <Content>Hello! from search results</Content>
+     <Footer />
+    </Container>
+   );
 
 export default results;
