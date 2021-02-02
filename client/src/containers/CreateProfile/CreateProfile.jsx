b5ded4733be5de27097d5f9dd78be9f2e5b3a526
Dennis Francis <dnfrancis22@gmail.com>
2021-02-01 21:31:19 -0500

    updated create profile

---
+++ client/src/containers/CreateProfile/CreateProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const createProfile = () => {
+    return (
+        <div>
+            <h1>a profile will go here</h1>
+        </div>
+    );
+};
+
+export default createProfile;
