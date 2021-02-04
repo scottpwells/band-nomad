90ba395c83f7af37c36104988137c1b5ae835257
Dennis Francis <dnfrancis22@gmail.com>
2021-02-04 10:35:47 -0500

    scafolded out the container files.

--- client/src/containers/CreateProfile/CreateProfile.jsx
+++ client/src/containers/CreateProfile/CreateProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const createProfile = () => {
     return (
         <div>
-            <h1>a profile will go here</h1>
+            <h1>This is where you create your profile.</h1>
         </div>
     );
 };
