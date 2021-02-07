0de089d7727d6d142bc628983bd7ad82eafe3213
Tara Martin <martin.tara117@gmail.com>
2021-02-07 18:22:38 -0500

    includes material-ui imports

--- client/src/containers/SearchResults/SearchResults.jsx
+++ client/src/containers/SearchResults/SearchResults.jsx
@@ -1,4 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 const axios = require("axios").default;
 
 const SearchResults = () => {
@@ -16,7 +30,7 @@ const SearchResults = () => {
   });
   return (
     <div>
-      <h1>This is the results page {musicians[0]? musicians[0].name:""}</h1>
+      <h1>This is the results page {musicians[0] ? musicians[0].name : ""}</h1>
     </div>
   );
 };
