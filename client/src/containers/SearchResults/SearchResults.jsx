b72f1059ce369666ff2a8ebab82255db9dd87449
Tara Martin <martin.tara117@gmail.com>
2021-02-07 17:50:00 -0500

    added useEffect to searchResults

--- client/src/containers/SearchResults/SearchResults.jsx
+++ client/src/containers/SearchResults/SearchResults.jsx
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 const axios = require("axios").default;
 
 const SearchResults = () => {
+  useEffect(() => {
+    axios.get("/api/musician").then(function (response) {
+      console.log(response);
+    });
+  });
   return (
     <div>
       <h1>This is the results page</h1>
