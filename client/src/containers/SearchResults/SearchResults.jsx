b9f3839a4cc961479ed2cc26dae5a85081d1da80
Tara Martin <martin.tara117@gmail.com>
2021-02-07 18:23:22 -0500

    removed material-ui

--- client/src/containers/SearchResults/SearchResults.jsx
+++ client/src/containers/SearchResults/SearchResults.jsx
@@ -1,18 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
 const axios = require("axios").default;
 
 const SearchResults = () => {
