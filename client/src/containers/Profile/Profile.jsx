c17db3707520fb2eb0bd2b3d6838a5ec19e4e506
Tara Martin <martin.tara117@gmail.com>
2021-02-09 11:59:35 -0500

    added axios, useState and useEffect to Profile.jsx

--- client/src/containers/Profile/Profile.jsx
+++ client/src/containers/Profile/Profile.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const axios = require("axios").default;
 
 const Profile = () => {
+  const [musicians, setMusicians] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/musician")
+      .then(function (response) {
+        setMusicians(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>This is the Profile page</h1>
