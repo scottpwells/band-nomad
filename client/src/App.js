ae642689ff23ca4bca9da4754a241c546e5b72f7
Tara Martin <martin.tara117@gmail.com>
2021-02-09 12:08:16 -0500

    added custom id to route path of profile in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/create" component={Create} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/login" component={Login} />
-          <Route path="/profile" component={Profile} />
+          <Route path="/profile/:id" component={Profile} />
           <Route exact path="/update" component={Update} />
           <Route exact path="/results" component={Results} />
         </Switch>
