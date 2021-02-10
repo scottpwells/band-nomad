e18966e90b2254d0224f768b97b99b12791f2314
Dennis Francis <dnfrancis22@gmail.com>
2021-02-09 19:29:04 -0500

    trying to display one profile

--- client/src/containers/UpdateProfile/UpdateProfile.jsx
+++ client/src/containers/UpdateProfile/UpdateProfile.jsx
@@ -5,7 +5,7 @@ import Form from "../../components/Form/Form";
 
 const UpdateProfile = () => {
     const history = useHistory();
-    
+
     const handleFormSubmit = (e, musicianData, id) => {
         e.preventDefault();
         axios
