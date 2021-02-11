26d41aecd569664c2f2b5abc7742c3ac50e93b06
Tara Martin <martin.tara117@gmail.com>
2021-02-11 18:10:12 -0500

    finised connecting contact page with the backend

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Profile = require("../models/contact");
+const Contact = require("../models/contact");
 
 router.post("/", (req, res) => {
   console.log(req.body);
-  contact.create(req.body).then((newContact) => {
+  Contact.create(req.body).then((newContact) => {
     console.log(newContact);
     res.json(newContact);
   });
