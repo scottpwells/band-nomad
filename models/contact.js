3282e832807709555f3de01f2bdc54f5943d8752
Tara Martin <martin.tara117@gmail.com>
2021-02-11 17:49:31 -0500

    added contact schema

---
+++ models/contact.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter your Name",
+  },
+  bandName: {
+    type: String,
+    trim: true,
+    required: "Please enter a band name",
+  },
+  genre: {
+    type: String,
+    trim: true,
+    required: "Please enter a genre",
+  },
+  email: {
+    type: String,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+  phone: {
+    type: Number,
+    trim: true,
+    required: "Please enter a phone number",
+  },
+  message: {
+    type: String,
+    trim: true,
+    required: "Please enter a message.",
+  },
+});
+
+const musician = mongoose.model("Musician", musicianSchema);
+
+module.exports = musician;
