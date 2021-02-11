fbd6fdea170341d43e4addcb5b934ca305cca7fb
Tara Martin <martin.tara117@gmail.com>
2021-02-11 17:55:27 -0500

    made another change to contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Profile = require("../models/musician");
+const Profile = require("../models/contact");
 
 router.post("/", (req, res) => {
   console.log(req.body);
