8f288d0d439f2ec5f28a007f2ac57f7bdec77a63
Dennis Francis <dnfrancis22@gmail.com>
2021-02-14 16:11:18 -0800

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar/NavBar.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
 import ContactPage from "./containers/ContactPage/ContactPage";
 import Create from "./containers/CreateProfile/CreateProfile";
 import Login from "./containers/Login/Login";
