88d1701facc4c0fa37680ce72b44dcf8e803c8f5
Dennis Francis <dnfrancis22@gmail.com>
2021-02-15 18:25:11 -0800

    replaced search fields with drop downs

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const Home = () => {
   const [instrument, setInstrument] = useState("");
   const [genre, setGenre] = useState("");
-  const [city, setCity] = useState("");
+  // const [city, setCity] = useState("");
   const [musicians, setMusicians] = useState([]);
 
   // const handleInputChange = (event) => {
@@ -15,7 +15,7 @@ const Home = () => {
   // };
 
   const handleFormSubmit = (event) => {
-    // When the form is submitted, prevent its default behavior, 
+    // When the form is submitted, prevent its default behavior,
     event.preventDefault();
     searchProfiles();
   };
@@ -29,9 +29,9 @@ const Home = () => {
       baseURL = baseURL + `genre=${genre}&`;
     }
 
-    if (city) {
-      baseURL = baseURL + `city=${city}&`;
-    }
+    // if (city) {
+    //   baseURL = baseURL + `city=${city}&`;
+    // }
     console.log(baseURL);
 
     axios
@@ -75,74 +75,76 @@ const Home = () => {
 
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ marginTop: 20 }}>
         <div className="Row">
-          <div className="col-sm-12">
-            <div className="input-field">
-              {/* TODO: Convert this to a dropdown for better instrument results */}
-              <input
-                id="instrument"
-                type="text"
-                value={instrument}
-                onChange={(e) => {
-                  setInstrument(e.target.value);
-                }}
-                placeholder="Instrument"
-              />
-              <input
-                id="genre"
-                type="text"
-                value={genre}
-                onChange={(e) => {
-                  setGenre(e.target.value);
-                }}
-                placeholder="Genre"
-              />
-              <input
-                id="city"
-                type="text"
-                value={city}
-                onChange={(e) => {
-                  setCity(e.target.value);
-                }}
-                placeholder="City"
-              />
-              <button
-                onClick={handleFormSubmit}
-                className="btn waves-effect waves-light"
-              >
-                Search Profiles
-              </button>
+          <div className="col s12">
+            <div className="Row">
+              <div className="col s12">
+                <select
+                  id="instrument"
+                  value={instrument}
+                  onChange={(e) => {
+                    setInstrument(e.target.value);
+                  }}
+                  className="browser-default"
+                >
+                  <option value="" disabled selected>
+                    Choose Instrument
+                  </option>
+                  <option value="bass">Bass Player</option>
+                  <option value="guitar">Guitar Player</option>
+                  <option value="drums">Drummer</option>
+                  <option value="singer">Lead Singer</option>
+                </select>
+              </div>
             </div>
+            <div className="row">
+              <div className="col s12">
+                <select
+                  placeholder="Genre"
+                  id="genre"
+                  name="genre"
+                  value={genre}
+                  onChange={(e) => {
+                    setGenre(e.target.value);
+                  }}
+                  className="browser-default"
+                >
+                  <option value="" disabled selected>
+                    Choose a Genre
+                  </option>
+                  <option value="blues">Rhythm and Blues</option>
+                  <option value="country">Country Music</option>
+                  <option value="jazz">Jazz Music</option>
+                  <option value="pop">Pop Music</option>
+                  <option value="rock">Rock Music</option>
+                </select>
+              </div>
+            </div>
+            <button
+              onClick={handleFormSubmit}
+              className="btn waves-effect waves-light"
+            >
+              Search Profiles
+            </button>
           </div>
         </div>
       </div>
-      {/* <div className="container">
+      <div className="container">
         <div className="row">
-          <div className="col">
-            <label>Browser Select</label>
-            <select className="browser-default">
-              <option value="" disabled selected>
-                Choose your option
-              </option>
-              <option value="1">Option 1</option>
-              <option value="2">Option 2</option>
-              <option value="3">Option 3</option>
-            </select>
+          <div className="col s12">
+            <table>
+              <tr>
+                <th>Profile Pic</th>
+                <th>Name</th>
+                <th>Instrument</th>
+                <th>Genre</th>
+                <th>City</th>
+              </tr>
+              {musicianRows}
+            </table>
           </div>
         </div>
-      </div> */}
-      <div>
-        <table>
-          <tr>
-            <th>Profile Pic</th>
-            <th>Name</th>
-            <th>Instrument</th>
-            <th>Genre</th>
-            <th>City</th>
-          </tr>
-          {musicianRows}
-        </table>
       </div>
     </>
   );
