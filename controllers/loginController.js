8297834a23099ee1676309d950324257922d0710
Tara Martin <martin.tara117@gmail.com>
2021-02-17 14:01:51 -0500

    added post for login in loginController

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Profile = require("../models/musician");
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
