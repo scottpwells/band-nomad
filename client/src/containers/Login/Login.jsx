1c0f5b644180402cabaa663e84270d4f8d286d12
Tara Martin <martin.tara117@gmail.com>
2021-02-19 14:07:43 -0500

    styled login page

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -30,14 +30,13 @@ function LogIn() {
 
   return (
     <>
-      <header></header>
       <div className="login col s6">
-        <img
-          style={{ Height: "auto" }}
+        {/* <img
+          style={{ height: "auto" }}
           id="navImage"
           src={logoBand100}
           alt="band nomad logo"
-        />
+        /> */}
         <h3 className="loginTitle">Login to Band Nomad</h3>
         <form onSubmit={handleFormSubmit}>
           <label for="Email">Email</label>
@@ -65,20 +64,27 @@ function LogIn() {
 function SignUp() {
   return (
     <div className="signup col s6">
+      {/* <div style={{ marginTop: "100px" }}> */}
       <h3 className="hello">Hello Friend!</h3>
-      <h5>Sign-up for Band Nomad and connect with other musicians! </h5>
+      <h5>Sign-up for Band Nomad </h5>
+      <div style={{ textAlign: "center" }}>
+        <h6>Start a band</h6>
+        <h6>Add a new band member</h6>
+        <h6>Connect with local musicians</h6>
+      </div>
       <Link to="create/">
         <button className="signUpButton btn waves-effect waves-light">
           Sign-Up!
         </button>
       </Link>
     </div>
+    // </div>
   );
 }
 
 const Login = () => {
   return (
-    <div className="row">
+    <div className="login-container row">
       <LogIn></LogIn>
       <SignUp></SignUp>
     </div>
