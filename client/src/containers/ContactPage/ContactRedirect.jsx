ae588147667830c381b79ab823f5eea82db9fc13
Tara Martin <martin.tara117@gmail.com>
2021-02-19 11:55:21 -0500

    added text to contactRedirect

--- client/src/containers/ContactPage/ContactRedirect.jsx
+++ client/src/containers/ContactPage/ContactRedirect.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+const ContactRedirect = () => {
+  return (
+    <>
+      <h3>Message Sent!</h3>
+      <h4>Redirecting you to the home page...</h4>
+    </>
+  );
+};
+
+export default ContactRedirect;
